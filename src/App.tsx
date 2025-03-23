681a4cc8dae5d7582df8e73e55b2318b5d169258
Gerson Aparecido <gerson.lima@yduqs.com.br>
2025-03-23 15:43:14 -0300

    Criando Formulario

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import Botao from './components/Botao';
+import Formulario from './components/Formulario';
+
 
 
 function App() {
   return (
     <div className="App">
-       <Botao />
+       <Formulario />
     </div>
   );
 }
