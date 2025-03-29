8f1c802af64dde8dd8f8da5e857f7d82aea72876
Gerson Aparecido <gerson.lima@yduqs.com.br>
2025-03-29 11:56:21 -0300

    Mostrando a tarefa selecionada

--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,10 +1,16 @@
+import React from 'react';
 import { ITarefa } from '../../types/tarefa';
 import Item from './Item';
 import style from './Lista.module.scss';
 
 
+interface Props {
+  tarefas: ITarefa[],
+  selecionaTarefa: (tarefaSelecionada: ITarefa) => void
 
-function Lista({ tarefas }: { tarefas: ITarefa[]}) {
+}
+
+function Lista({ tarefas, selecionaTarefa }: Props) {
   
   return (
     <aside className={style.listaTarefas}>
@@ -12,10 +18,11 @@ function Lista({ tarefas }: { tarefas: ITarefa[]}) {
         Estudos do dia 
       </h2>
       <ul>
-        {tarefas.map((item, index) => (
+        {tarefas.map(item => (
           <Item
-            key={index}
-            {...item}
+              selecionaTarefa={selecionaTarefa}
+              key={item.id}
+              {...item}
           />
         ))}
       </ul>
