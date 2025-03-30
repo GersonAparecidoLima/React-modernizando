aa7113071bf41dfb8ee352b285b101bf0c08e59f
Gerson Aparecido <gerson.lima@yduqs.com.br>
2025-03-30 09:25:05 -0300

    Refatorando Class Components

--- src/components/Botao/index.tsx
+++ src/components/Botao/index.tsx
@@ -1,23 +1,22 @@
 import React from 'react';
 import style from './Botao.module.scss';
 
-class Botao extends React.Component<{
-  type?: "button" | "submit" | "reset" | undefined;
+interface Props {
+  type?: "button" | "submit" | "reset" | undefined,
   onClick?: () => void,
-  children?: React.ReactNode;  // Adicionando children explicitamente
-}> {
-  render() {
-    const { type = "button", onClick } = this.props;
-    return (
-      <button
+  children?: React.ReactNode
+}
+
+function Botao({ onClick, type, children }: Props) {
+  return (
+    <button
       onClick={onClick}
       type={type}
       className={style.botao}
     >
-        {this.props.children}
-      </button>
-    );
-  }
+      {children}
+    </button>
+  )
 }
 
 export default Botao;
